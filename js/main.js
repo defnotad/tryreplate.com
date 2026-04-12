53b88b9c2086c672bc274e6423294ee1b5135079
defnotad <aditya.arya.ggn@gmail.com>
2026-04-12 12:37:59 +0530

    2025 to 2026

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Fade in on scroll
+const els = document.querySelectorAll('.fade-in');
+const obs = new IntersectionObserver((entries) => {
+  entries.forEach((e, i) => {
+    if (e.isIntersecting) {
+      setTimeout(() => e.target.classList.add('visible'), i * 80);
+      obs.unobserve(e.target);
+    }
+  });
+}, { threshold: 0.1 });
+els.forEach(el => obs.observe(el));
